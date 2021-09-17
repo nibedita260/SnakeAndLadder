81cd57499b7dd1f26484cc4ad576e8ccf79e2dfa
nibedita260 <deepi.nibedita260.com>
2021-09-17 20:11:14 +0530

    [Nibedita]Add:Till MaxPos PlayerPosition SnakeAndLadder Feature Implemented

--- SnakeAndLadder/Player.cs
+++ SnakeAndLadder/Player.cs
@@ -6,33 +6,45 @@ namespace SnakeAndLadder
 {
     class Player
     {
-        const int No_PLAY = 1, LADDER = 2, SNAKE = 3;
-        int playerPosition = 0;
+        const int No_PLAY = 1, LADDER = 2, SNAKE = 3,MAX_PLAYER_POSITION=100;
+        int playerPosition = 0,count=0;
         
         public void play()
         {
             Random random = new Random();
-            int rollDie = random.Next(1, 7);
+            int rollDie = random.Next(1, 6);
             int options = random.Next(1, 3);
             Console.WriteLine("Afer rolls a die , the number get :" + options);
-            switch (options)
+            while (playerPosition < MAX_PLAYER_POSITION)
             {
-                
-               case LADDER:
-                    Console.WriteLine("Ladder");
-                    playerPosition += rollDie;
-                    break;
-                case SNAKE:
-                    Console.WriteLine("Snake");
-                    playerPosition -= rollDie;
-                    break;
-                case No_PLAY:
-                    Console.WriteLine("No PLAY");
-                    playerPosition = 0;
-                    break;
-                default:
-                    break;
+                count++;
+                do {
+                    switch (options)
+                    {
+                        case LADDER:
+                            Console.WriteLine("Ladder");
+                            playerPosition += rollDie;
+                            break;
+                        case SNAKE:
+                            Console.WriteLine("Snake");
+                            playerPosition -= rollDie;
+                            break;
+                        case No_PLAY:
+                            Console.WriteLine("No PLAY");
+                            //playerPosition = 0;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (playerPosition < 0)
+                    {
+                        playerPosition = 0;
+                    }
+                    Console.WriteLine("Player position till reaches the points are:" + playerPosition);
+                } while (playerPosition!=100 && options==1);
             }
+           
             Console.WriteLine("After rolled a dice ,player position:" + playerPosition);
 
         }
