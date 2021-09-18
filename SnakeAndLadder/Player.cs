8dee9fd55ec1bc6be0828fa8cd954905a86227e2
nibedita260 <deepi.nibedita260.com>
2021-09-18 12:59:35 +0530

    [Nibedita]Add:Cond For PlayerPosition SnakeAndLadd Feature Implemented

--- SnakeAndLadder/Player.cs
+++ SnakeAndLadder/Player.cs
@@ -7,19 +7,17 @@ namespace SnakeAndLadder
     class Player
     {
         const int No_PLAY = 1, LADDER = 2, SNAKE = 3,MAX_PLAYER_POSITION=100;
-        int playerPosition = 0,count=0;
+        int playerPosition = 0;
         
         public void play()
         {
             Random random = new Random();
-            int rollDie = random.Next(1, 6);
-            int options = random.Next(1, 3);
+            int rollDie = random.Next(1, 7);
+            int options = random.Next(1, 4);
             Console.WriteLine("Afer rolls a die , the number get :" + options);
             while (playerPosition < MAX_PLAYER_POSITION)
             {
-                count++;
-                do {
-                    switch (options)
+                switch (options)
                     {
                         case LADDER:
                             Console.WriteLine("Ladder");
@@ -31,22 +29,26 @@ namespace SnakeAndLadder
                             break;
                         case No_PLAY:
                             Console.WriteLine("No PLAY");
-                            //playerPosition = 0;
+                            playerPosition = 0;
                             break;
                         default:
+                            playerPosition = 0;
                             break;
-                    }
+                }
 
-                    if (playerPosition < 0)
-                    {
-                        playerPosition = 0;
-                    }
-                    Console.WriteLine("Player position till reaches the points are:" + playerPosition);
-                } while (playerPosition!=100 && options==1);
+                //due to player position 0 or less than zero, the loop runs infine times
+                if (playerPosition <= 0)
+                {
+                    playerPosition = 0;
+                    Console.WriteLine("Player position starts from zero");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Player postion stays in same position");
+                }
             }
-           
             Console.WriteLine("After rolled a dice ,player position:" + playerPosition);
-
         }
     }
 }
